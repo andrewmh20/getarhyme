a6d581290f7753b66a8686081650ddf742fed6de
Tzvi (Andrew) Merczynski-Hait <andrewmh20@users.noreply.github.com>
2020-06-15 17:53:12 -0400

    html and js

---
+++ script.js
@@ -0,0 +1,64 @@
+
+const api = "https://rhymebrain.com/talk?function=getRhymes"
+
+function getRhymes() {
+
+    document.getElementById("rhymeNode").innerText = "";
+    document.getElementById("errorText").innerText = ""
+
+    document.getElementById("rhymeLoading").innerText = "Your Rhyme is Loading..."
+
+
+    const input = document.getElementById("RhymeBrainInput");
+    const url = api + "&word=" + input.value;
+
+    fetch(url).then(
+        (resp) => resp.json()
+    ).then((data) =>
+        insertRhyme(data)
+    ).catch((err) => {
+        handleRhymeError(err)
+    }
+    )
+
+}
+
+function insertRhyme(data) {
+    let rhymeToShow = null;
+    console.log(data)
+
+    if (Array.isArray(data)) {
+        if (data.length > 0) {
+            let rhymeIndex = getRndInteger(0, data.length - 1);
+            rhymeToShow = data[rhymeIndex].word;
+        }
+        else {
+            handleRhymeError("We couldn't find any rhymes!")
+        }
+    } else {
+        if (data) {
+            rhymeToShow = data.word;
+        } else handleRhymeError("There was a problem! You can try again or give up.");
+
+    }
+    document.getElementById("rhymeLoading").innerText = "";
+    document.getElementById("rhymeNode").innerText = rhymeToShow;
+
+
+
+
+}
+
+function handleRhymeError(err) {
+
+    console.log(err);
+    let errText = "There was a problem! You can try again or give up."
+    document.getElementById("rhymeLoading").innerText = "";
+    document.getElementById("rhymeNode").innerText = "";
+    document.getElementById("errorText").innerText = errText;
+
+}
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
